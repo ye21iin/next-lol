b5e59daa452220422e473ec2b684e1c80f34a43c
ye21iin <kathykr120@gmail.com>
2025-03-11 21:34:45 +0900

    feat: data fetching / render list #1

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,5 +1,50 @@
-const Champions = () => {
-  return <div>Champions page</div>;
+import { Champion, VersionList } from "@/types/Champion";
+import Image from "next/image";
+
+const Champions = async () => {
+  const version = await getLatestVersion();
+
+  if (!version) return;
+
+  const champions: Record<string, Champion> = await getChampionList(version);
+
+  return (
+    <div>
+      <div className="grid grid-cols-4 gap-4">
+        {champions &&
+          Object.keys(champions).map((e) => (
+            <div key={e} className="border">
+              <Image
+                src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champions[e].image.full}`}
+                alt="image"
+                width={48}
+                height={48}
+              />
+              <h2 className="mt-2 text-xl font-semibold">
+                {champions[e].name}
+              </h2>
+              <p className="text-gray-500">{champions[e].title}</p>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Champions;
+
+const getLatestVersion = async () => {
+  const res = await fetch(
+    "https://ddragon.leagueoflegends.com/api/versions.json"
+  );
+  const versions: VersionList = await res.json();
+  return versions[0];
+};
+
+const getChampionList = async (version: string) => {
+  const res = await fetch(
+    `https://ddragon.leagueoflegends.com/cdn/${version}/data/ko_KR/champion.json`
+  );
+  const json = await res.json();
+  return json.data;
+};
---
+++ src/types/Champion.ts
@@ -0,0 +1,57 @@
+export type VersionList = string[];
+
+interface ChampionInfo {
+  attack: number;
+  defense: number;
+  magic: number;
+  difficulty: number;
+}
+
+interface ChampionImage {
+  full: string;
+  sprite: string;
+  group: string;
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+}
+
+interface ChampionStats {
+  hp: number;
+  hpperlevel: number;
+  mp: number;
+  mpperlevel: number;
+  movespeed: number;
+  armor: number;
+  armorperlevel: number;
+  spellblock: number;
+  spellblockperlevel: number;
+  attackrange: number;
+  hpregen: number;
+  hpregenperlevel: number;
+  mpregen: number;
+  mpregenperlevel: number;
+  crit: number;
+  critperlevel: number;
+  attackdamage: number;
+  attackdamageperlevel: number;
+  attackspeedperlevel: number;
+  attackspeed: number;
+}
+
+export interface Champion {
+  version: string;
+  id: string;
+  key: string;
+  name: string;
+  title: string;
+  blurb: string;
+  info: ChampionInfo;
+  image: ChampionImage;
+  tags: string[];
+  partype: string;
+  stats: ChampionStats;
+}
+
+export type ChampionDetail = {};
