d08daef0515d8db4c4dd450c5b08c287fcfc09f0
ye21iin <kathykr120@gmail.com>
2025-03-12 15:13:12 +0900

    fix: common layout - fix href route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       >
         <div className="flex justify-evenly items-center h-14">
           <Link href="/">홈</Link>
-          <Link href="champions">챔피언 목록</Link>
+          <Link href="/champions">챔피언 목록</Link>
           <Link href="/items">아이템 목록</Link>
           <Link href="/rotation">챔피언 로테이션</Link>
         </div>
