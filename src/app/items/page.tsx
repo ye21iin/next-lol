5afce355321197bc51b4549fcb8d5a0528f0b91b
ye21iin <kathykr120@gmail.com>
2025-03-12 14:17:56 +0900

    feat: update UI structure #2

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -14,7 +14,10 @@ const Items = async () => {
       <div className="grid grid-cols-4 gap-4">
         {items &&
           Object.keys(items).map((e) => (
-            <div key={e} className="border">
+            <div
+              key={e}
+              className="border flex flex-col justify-center items-center"
+            >
               <Image
                 src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/item/${items[e].image.full}`}
                 alt="image"
@@ -22,9 +25,14 @@ const Items = async () => {
                 height={48}
               />
               <h2 className="mt-2 text-xl font-semibold">{items[e].name}</h2>
-              <p className="text-gray-500">
-                {items[e].description.replace(/<[^>]*>/g, "")}
+              <p className="text-gray-500">가격: {items[e].gold.base}</p>
+              <p className="text-gray-500">팔때: {items[e].gold.sell}</p>
+              <p className="text-red-500">
+                {!items[e].gold.purchasable && "구매 불가"}
               </p>
+              {items[e].description && (
+                <p className="bg-slate-300">아이템 설명</p>
+              )}
             </div>
           ))}
       </div>
