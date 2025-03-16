514ca3665d126783efa2cdb7ef9ef834ed6e8cb6
ye21iin <kathykr120@gmail.com>
2025-03-17 01:13:09 +0900

    feat: rotation UI setup #3 - with Card component reused

--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { useEffect, useState } from "react";
 import { GET } from "../api/rotation/route";
+import Card from "@/components/Card";
+import { VERSION } from "@/lib/constant";
+import { Champion } from "@/types/Champion";
 
 const Rotation = () => {
   interface Dataset {
@@ -11,6 +14,7 @@ const Rotation = () => {
 
   const [dataset, setDataset] = useState<Dataset | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
+  const [champions, setChampions] = useState<Record<string, Champion>>({});
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,6 +22,23 @@ const Rotation = () => {
       try {
         const response = await fetch("/api/rotation").then((res) => res.json());
         setDataset(response);
+
+        const resChampion = await fetch(
+          `https://ddragon.leagueoflegends.com/cdn/${VERSION}/data/ko_KR/champion.json`,
+        );
+        const allChampions = await resChampion.json();
+
+        const rotationChampions: Record<string, Champion> = {};
+        response.freeChampionIds.forEach((id: number) => {
+          const champKey = Object.keys(allChampions.data).find(
+            (key) => allChampions.data[key].key === String(id),
+          );
+          if (champKey) {
+            rotationChampions[champKey] = allChampions.data[champKey];
+          }
+        });
+
+        setChampions(rotationChampions);
       } catch (err) {
         console.error(err);
       } finally {
@@ -31,10 +52,24 @@ const Rotation = () => {
 
   return (
     <div>
-      Rotation page
-      <ul>
-        {dataset && dataset.freeChampionIds.map((id) => <li key={id}>{id}</li>)}
-      </ul>
+      <h1 className="ml-4 text-3xl font-bold">이번 주 무료 챔피언</h1>
+      <div className="grid grid-cols-2 gap-4 p-4 md:grid-cols-4 lg:grid-cols-6">
+        {dataset?.freeChampionIds.map((id) => {
+          const champKey = Object.keys(champions).find(
+            (key) => champions[key].key === String(id),
+          );
+
+          return (
+            <div key={id}>
+              {champKey ? (
+                <Card champion={champions[champKey]} />
+              ) : (
+                <p>Loading...</p>
+              )}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
