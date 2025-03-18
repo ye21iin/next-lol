d7341d8a775aca587403aaea079101909728f364
ye21iin <kathykr120@gmail.com>
2025-03-18 12:52:55 +0900

    fix: warning resolve

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,11 +1,21 @@
 "use client";
 
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
+  const [isMount, setMount] = useState(false);
+
+  useEffect(() => {
+    setMount(true);
+  }, []);
+
+  if (!isMount) {
+    return null;
+  }
+
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
